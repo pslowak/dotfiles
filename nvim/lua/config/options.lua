2c5f3743499b200266fd8a29ed457fab0636f872
Philipp Slowak <philipp.slowak@tuwien.ac.at>
2026-07-07 21:47:31 +0200

    feat(nvim): add options.lua

---
+++ nvim/lua/config/options.lua
@@ -0,0 +1,29 @@
+local opt = vim.opt
+
+-- Display
+opt.cursorline = true
+opt.number = true
+opt.relativenumber = true
+opt.scrolloff = 5
+opt.termguicolors = true
+
+-- Indenting
+opt.expandtab = true
+opt.shiftwidth = 4
+opt.smartindent = true
+opt.softtabstop = 4
+opt.tabstop = 4
+
+-- Search
+opt.ignorecase = true
+opt.incsearch = true
+opt.smartcase = true
+
+-- Interaction
+opt.clipboard = "unnamedplus"
+opt.mouse = "a"
+opt.splitbelow = true
+opt.splitright = true
+
+-- Persistence
+opt.undofile = true
