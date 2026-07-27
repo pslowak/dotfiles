fcf83e15e38a82bd02651b36ae18e886967d2010
Philipp Slowak <philipp.slowak@tuwien.ac.at>
2026-07-27 18:00:38 +0200

    feat(nvim): add telescope plugin

---
+++ nvim/lua/plugins/telescope.lua
@@ -0,0 +1,13 @@
+return {
+  "nvim-telescope/telescope.nvim",
+  dependencies = { "nvim-lua/plenary.nvim" },
+  cmd = "Telescope",
+  keys = {
+    { "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "Find files" },
+    { "<leader>fg", "<cmd>Telescope live_grep<cr>", desc = "Live grep" },
+    { "<leader>fb", "<cmd>Telescope buffers<cr>", desc = "Buffers" },
+    { "<leader>fh", "<cmd>Telescope help_tags<cr>", desc = "Help tags" },
+    { "<leader>fr", "<cmd>Telescope lsp_references<cr>", desc = "LSP references" },
+    { "<leader>fs", "<cmd>Telescope lsp_document_symbols<cr>", desc = "Document symbols" },
+  },
+}
